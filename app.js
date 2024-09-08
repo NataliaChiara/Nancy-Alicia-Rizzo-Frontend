871d4a2eef40e6136eda87541195029a4d39e5fb
Natalia Chiara <natalia@contenidos-digitales.com>
2024-09-07 21:11:03 -0300

    imagenes desde sql

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const titulo = document.getElementById('titulo');
 const sobreMi = document.getElementById('sobre-mi');
 const tabla = document.getElementById('tabla')
 const containerTecnicas = document.getElementById('container-tecnicas')
+const testimoniosContainer = document.getElementById('testimonios')
 
 getInfo()
   .then(info => {
-    const {psicologo, servicios, terapias} = info
+    const {psicologo, servicios, terapias, testimonios} = info
+
+    console.log(info)
 
     menu.innerHTML = `
       <a href="tel:${psicologo.telefono}" class="contacto">
@@ -23,8 +26,11 @@ getInfo()
       <h1>${psicologo.subtitulo}</h1>
     `
     sobreMi.innerHTML = `
-      <h3>Sobre Mi</h3>
-      <p>${psicologo.sobre_mi}</p>
+      <div class="sobre-mi-2">
+        <h3>Sobre Mi</h3>
+        <p>${psicologo.sobre_mi}</p>
+      </div>
+      <img src="${psicologo.picture_url}" alt="Foto psicóloga" width="300px">
     `
 
     servicios.forEach((item) => {
@@ -36,9 +42,15 @@ getInfo()
     terapias.forEach((item) => {
       containerTecnicas.innerHTML += `
         <div class="tecnicas">
-          <img src="/imagenes/terapia.jpg" width="200px">
+          <img src="${item.url}" width="200px">
           <p>${item.titulo}<br><br>${item.descripcion}</p>
         </div>
+      `
+    })
+
+    testimonios.forEach((item) => {
+      testimoniosContainer.innerHTML += `
+        <img src="${item.url}" alt="Reseña de google">
       `
     })
 
